a9dbba1b4e019555b12a594ea692d40e338869b8
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-03-20 09:45:37 +0100

    updated student name

--- stud.py
+++ stud.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 def studentsGrades():
+	studname = 'John'
 	math = 83
 	science = 84
 	english = 85
 	sum = (math + science + english)
 	average = sum/3
-	sentence = 'Grades of John in Math:'+str(math)+". Science:"+str(science)+". And English:"+str(english)
+	sentence = 'Grades of '+studname+' in Math:'+str(math)+". Science:"+str(science)+". And English:"+str(english)
 	print(sentence)
 	sentence2 = 'Average:'+str(average)
 	print(sentence2)
